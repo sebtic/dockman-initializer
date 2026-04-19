d020c723bfe2274d83b867ccf0257e5ff3bfe46f
Sébastien Aupetit <sebtic@users.noreply.github.com>
2026-04-19 11:48:38 +0200

    Initial import

---
+++ init.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+export BASE_DIR="$PWD"
+
+if [ ! -d .git ]; then
+    git init .
+fi
+
+cat <<EOF > .gitignore
+# do not version override directories since it contains secrets and specific configuration
+override
+
+# generated files
+generated
+
+# data
+data
+EOF
+
+git add .gitignore
+
+cat <<'EOF' > dockman
+#!/bin/bash
+
+set -e
+
+export BASE_DIR="$(dirname "$(readlink -f "$0")")"
+
+source $BASE_DIR/bin/dockman.sh
+EOF
+chmod +x dockman
+
+git add dockman
+
+if [ ! -d bin ]; then 
+    git submodule add https://github.com/sebtic/dockman-bin.git bin
+fi
+
+if [ ! -d global ]; then
+    git submodule add https://github.com/sebtic/dockman-global.git global
+fi
+
+mkdir -p stacks override
+
+touch stacks/.keep
+
+git add stacks/.keep
+
+
+
